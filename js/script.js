2cf99200076a854e84fee0b31708ebca1cb9767c
VictoriaDewito <vikatereshonok@mail.ru>
2022-07-29 20:12:29 +0000

    Nabekalas'

--- js/script.js
+++ js/script.js
@@ -10,5 +10,34 @@ $('#cssmenu').prepend('<div id="menu-button">Menu</div>');
 			menu.addClass('open');
 		}
 	});
+
+	const draw_flower = function(ctx) {
+		ctx.fillStyle = 'rgb(40,30,60)';
+		ctx.beginPath();
+		ctx.arc(100,100,50,0, 2*Math.PI);
+		ctx.arc(100,50,25,0, 2*Math.PI);
+		ctx.arc(50,100,25,0, 2*Math.PI);
+		ctx.arc(150,100,25,0, 2*Math.PI);
+		ctx.arc(100,150,25,0, 2*Math.PI);
+
+		ctx.fill();
+	};
+
+	$('#bekard').on('click', function(){
+		// const canvas = $('#bekard-canvas');
+		const canvas = document.getElementById('bekard-canvas');
+		if (canvas.getContext) {
+			const ctx = canvas.getContext('2d');
+			draw_flower(ctx);
+			// ctx.fillStyle = 'rgb(40,30,60)';
+			// ctx.fillRect(10, 10, 50, 50);
+
+		} else {
+			console.log('BEEEEEK');
+		}
+
+		console.log("Giga-bekard");
+	});
+
 });
 } )( jQuery );
