7102515cd07af1c39fa3d35723d2d9697c5cb6a4
tereshonok <vikatereshonok@mail.ru>
2022-08-29 19:06:51 +0000

    Лучший бек в мире

--- js/script.js
+++ js/script.js
@@ -1,43 +1,27 @@
-( function( $ ) {
-$( document ).ready(function() {
-$('#cssmenu').prepend('<div id="menu-button">Menu</div>');
-	$('#cssmenu #menu-button').on('click', function(){
-		var menu = $(this).next('ul');
-		if (menu.hasClass('open')) {
-			menu.removeClass('open');
-		}
-		else {
-			menu.addClass('open');
-		}
-	});
+function draw_circle(ctx, x, y, r) {
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2*Math.PI);
+	ctx.fill();
+}
 
-	const draw_flower = function(ctx) {
-		ctx.fillStyle = 'rgb(40,30,60)';
-		ctx.beginPath();
-		ctx.arc(100,100,50,0, 2*Math.PI);
-		ctx.arc(100,50,25,0, 2*Math.PI);
-		ctx.arc(50,100,25,0, 2*Math.PI);
-		ctx.arc(150,100,25,0, 2*Math.PI);
-		ctx.arc(100,150,25,0, 2*Math.PI);
+function draw_flower(ctx, x, y, r) {
+	ctx.fillStyle = 'rgb(40,30,60)';
+	var r2 = r / 2;
+	draw_circle(ctx, x, y, r);
+	draw_circle(ctx, x, y - r, r2);
+	draw_circle(ctx, x - r, y, r2);
+	draw_circle(ctx, x + r, y, r2);
+	draw_circle(ctx, x, y + r, r2);
+};
 
-		ctx.fill();
-	};
-
-	$('#bekard').on('click', function(){
-		// const canvas = $('#bekard-canvas');
-		const canvas = document.getElementById('bekard-canvas');
-		if (canvas.getContext) {
-			const ctx = canvas.getContext('2d');
-			draw_flower(ctx);
-			// ctx.fillStyle = 'rgb(40,30,60)';
-			// ctx.fillRect(10, 10, 50, 50);
-
-		} else {
-			console.log('BEEEEEK');
-		}
-
-		console.log("Giga-bekard");
-	});
-
-});
-} )( jQuery );
+function bekard_animate() {
+	const canvas = document.getElementById('bekard-canvas');
+	if (canvas.getContext) {
+		const ctx = canvas.getContext('2d');
+		var width = canvas.width;
+		var height = canvas.height;
+		draw_flower(ctx, width / 2, height / 2, 50);
+	} else {
+		console.log('No Canvas context found');
+	}
+}
